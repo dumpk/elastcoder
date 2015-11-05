24ddfc851ce8d6b4dcc56db224ebb1554d7a7cd7
Nicolás Bistolfi <nbistolfi@gmail.com>
2015-11-05 11:04:12 -0300

    Update ElastcoderAWS with EsetresAWS lib

--- src/ElastcoderAWS.php
+++ src/ElastcoderAWS.php
@@ -4,6 +4,7 @@ namespace Dumpk\Elastcoder;
 
 use Aws\S3\S3Client;
 use Aws\ElasticTranscoder\ElasticTranscoderClient;
+use Dumpk\Esetres\EsetresAWS;
 
 class ElastcoderAWS
 {
@@ -27,15 +28,6 @@ class ElastcoderAWS
         return $config;
     }
 
-    private function _s3c()
-    {
-        if (!isset($this->s3c)) {
-            $this->s3c = new S3Client($this->_getConfig());
-        }
-
-        return $this->s3c;
-    }
-
     public function listJobs($PipelineId, $page = null)
     {
         return $this->etc->listJobsByPipeline([
@@ -43,19 +35,6 @@ class ElastcoderAWS
             'PageToken' => $page,
             'PipelineId' => $PipelineId, // REQUIRED
         ]);
-    }
-
-    public function uploadFile($localPath, $key, $bucket, $acl = 'public-read', $metadata = array(), $cache = 'max-age=3600', $extraOptions = array()) {
-        $s3c = $this->_s3c();
-        $objectOptions = [
-            'ACL'           => $acl,
-            'Bucket'        => $bucket,
-            'Key'           => $key,
-            'CacheControl'  => $cache,
-            'SourceFile'    => $localPath,
-        ];
-        $result = $s3c->putObject(array_merge($objectOptions, $extraOptions));
-        return $result;
     }
 
     public function transcodeVideo($inputKey, $destinationKey, $config, $thumbPattern = null)
@@ -101,68 +80,51 @@ class ElastcoderAWS
         }
     }
 
-    public function getObject($key, $bucket)
-    {
-        $s3c = $this->_s3c();
-        $object = null;
-        if ($s3c->doesObjectExist($bucket, $key)) {
-            $object = $s3c->getObject(array('Bucket' => $bucket, 'Key' => $key));
-        }
+    /**
+    * @deprecated
+    */
+    public function uploadFile($localPath, $key, $bucket, $acl = 'public-read', $metadata = array(), $cache = 'max-age=3600', $extraOptions = array()) {
 
-        return $object;
-    }
-
-    public function deleteObject($key, $bucket)
-    {
-        $s3c = $this->_s3c();
-        $params = [
-            'Bucket' => $bucket,
-            'Key'    => $key,
-        ];
-        if ($s3c->deleteObject($params)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function objectExists($key, $bucket)
-    {
-        $s3c = $this->_s3c();
-        if ($s3c->doesObjectExist($bucket, $key)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function setPublicObject($key, $bucket)
-    {
-        return $this->setObjectACL($key, $bucket);
+        return EsetresAWS::uploadFile($localPath, $key, $bucket, $acl, $metadata, $cache, $extraOptions);
     }
 
     /**
-     * setObjectACL
-     * Change the access level of an object on S3.
-     *
-     * @param $key
-     * @param $bucket
-     * @param $acl (private|public-read|public-read-write|authenticated-read|bucket-owner-read|bucket-owner-full-control)
-     */
+    * @deprecated
+    */
+    public function getObject($key, $bucket)
+    {
+        return EsetresAWS::getObject($key, $bucket);
+    }
+
+    /**
+    * @deprecated
+    */
+    public function deleteObject($key, $bucket)
+    {
+        return EsetresAWS::deleteObject($key, $bucket);
+    }
+
+    /**
+    * @deprecated
+    */
+    public function objectExists($key, $bucket)
+    {
+        return EsetresAWS::objectExists($key, $bucket);
+    }
+
+    /**
+    * @deprecated
+    */
+    public function setPublicObject($key, $bucket)
+    {
+        return EsetresAWS::setPublicObject($key, $bucket);
+    }
+
+    /**
+    * @deprecated
+    */
     public function setObjectACL($key, $bucket, $acl = 'public-read')
     {
-        $s3c = $this->_s3c();
-        if ($s3c->doesObjectExist($bucket, $key)) {
-            $result = $s3c->putObjectAcl(array(
-                'ACL' => $acl,
-                'Bucket' => $bucket,
-                'Key' => $key,
-            ));
-            if ($result) {
-                return true;
-            }
-        }
-
-        return false;
+        return EsetresAWS::setObjectACL($key, $bucket, $acl);
     }
 }
